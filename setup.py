741aa049a98930bdd3697589854390941adb2977
a-kobori <89172056+a-kobori@users.noreply.github.com>
2023-08-06 16:39:45 +0900

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="blog-tool",
+    version='1.0',
+    description='',
+    author='',
+    author_email='',
+    url='https://github.com/koboriakira/blog_tool',
+    packages=find_packages(),
+    entry_points="""
+      [console_scripts]
+      blog-tool = blog_tool.cli:execute
+    """,
+    install_requires=open('requirements.txt').read().splitlines(),
+    # license='MIT',
+)
---
+++ tests/test_main.py
@@ -0,0 +1,5 @@
+from blog_tool import main
+
+
+def test_multiply():
+    assert main.multiply(2, 3) == 6
